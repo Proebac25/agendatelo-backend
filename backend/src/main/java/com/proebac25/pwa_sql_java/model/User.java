8713bc600724f34c8f44861eb49c420fcdf13f26
Miguel García Durán <miguel@estanoche.es>
2025-11-12 22:35:56 +0100

    Fix User.id to Long with IDENTITY

--- backend/src/main/java/com/proebac25/pwa_sql_java/model/User.java
+++ backend/src/main/java/com/proebac25/pwa_sql_java/model/User.java
@@ -8,9 +8,8 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
     @Column(nullable = false, unique = true)
     private String email;
 
