9dc267d2ff15b8a3f1f8f0b234b1373b33cea98d
Miguel García <proebac25@gmail.com>
2025-11-13 23:39:33 +0100

    Delete backend/src/main/java/com/proebac25/pwa_sql_java/repository/UserRepository.java

--- backend/src/main/java/com/proebac25/pwa_sql_java/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proebac25.pwa_sql_java.repository;
-
-import com.proebac25.pwa_sql_java.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
